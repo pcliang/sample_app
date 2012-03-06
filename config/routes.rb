e1cca9909daf6284756cbc4abb681824f4ff1999
Administrator <administrator@ubuntu-vm.(none)>
2012-03-06 17:28:23 +0000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  root :to => 'pages#home
+  root :to => 'pages#home'
 
-#  get "pages/home"
-
-#  get "pages/contact"
-
-#  get "pages/about"
-
-#  get "pages/help"
+  get "/pages/home"
+  get "/pages/contact"
+  get "/pages/about"
+  get "/pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
